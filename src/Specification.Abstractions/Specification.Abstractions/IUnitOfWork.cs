f641fbc3e67905f9fc2161ca66747e982dace197
Alejandro <alejandro.sanchez.bastida@gmail.com>
2022-09-24 20:09:14 +0200

    Use file-scoped namespaces

--- src/Specification.Abstractions/Specification.Abstractions/IUnitOfWork.cs
+++ src/Specification.Abstractions/Specification.Abstractions/IUnitOfWork.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Specification.Abstractions;
 
-namespace Specification.Abstractions
+public interface IUnitOfWork
 {
-    public interface IUnitOfWork
-    {
-        ValueTask<TEntity?> GetById<TEntity>(object?[]? id, CancellationToken cancellationToken = default) where TEntity : class;
-        Task<List<TEntity>> List<TEntity>(CancellationToken cancellationToken = default) where TEntity : class;
-        Task<List<TProjection>> List<TEntity, TProjection>(Query<TEntity, TProjection> query, CancellationToken cancellationToken = default) where TEntity : class;
-        Task<TEntity?> FirstOrDefault<TEntity>(CancellationToken cancellationToken = default) where TEntity : class;
-        Task<TProjection?> FirstOrDefault<TEntity, TProjection>(Query<TEntity, TProjection> query, CancellationToken cancellationToken = default) where TEntity : class;
-        void Add<TEntity>(TEntity entity) where TEntity : class;
-        void Update<TEntity>(TEntity entity) where TEntity : class;
-        void Delete<TEntity>(TEntity entity) where TEntity : class;
-    }
+    ValueTask<TEntity?> GetById<TEntity>(object?[]? id, CancellationToken cancellationToken = default) where TEntity : class;
+    Task<List<TEntity>> List<TEntity>(CancellationToken cancellationToken = default) where TEntity : class;
+    Task<List<TProjection>> List<TEntity, TProjection>(Query<TEntity, TProjection> query, CancellationToken cancellationToken = default) where TEntity : class;
+    Task<TEntity?> FirstOrDefault<TEntity>(CancellationToken cancellationToken = default) where TEntity : class;
+    Task<TProjection?> FirstOrDefault<TEntity, TProjection>(Query<TEntity, TProjection> query, CancellationToken cancellationToken = default) where TEntity : class;
+    void Add<TEntity>(TEntity entity) where TEntity : class;
+    void Update<TEntity>(TEntity entity) where TEntity : class;
+    void Delete<TEntity>(TEntity entity) where TEntity : class;
 }
--- src/Specification.Abstractions/Specification/UnitOfWork.cs
+++ src/Specification.Abstractions/Specification/UnitOfWork.cs
@@ -1,59 +1,58 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Specification.Abstractions;
 
-namespace Specification
+namespace Specification;
+
+public class UnitOfWork : IUnitOfWork
 {
-    public class UnitOfWork : IUnitOfWork
+    private readonly DbContext _context;
+
+    public UnitOfWork(DbContext context)
     {
-        private readonly DbContext _context;
+        _context = context;
+    }
 
-        public UnitOfWork(DbContext context)
-        {
-            _context = context;
-        }
+    public ValueTask<TEntity?> GetById<TEntity>(object?[]? id, CancellationToken cancellationToken = default) where TEntity : class
+    {
+        return _context.FindAsync<TEntity>(id, cancellationToken: cancellationToken);
+    }
 
-        public ValueTask<TEntity?> GetById<TEntity>(object?[]? id, CancellationToken cancellationToken = default) where TEntity : class
-        {
-            return _context.FindAsync<TEntity>(id, cancellationToken: cancellationToken);
-        }
+    public Task<List<TEntity>> List<TEntity>(CancellationToken cancellationToken = default) where TEntity : class
+    {
+        return _context.Set<TEntity>().ToListAsync(cancellationToken);
+    }
 
-        public Task<List<TEntity>> List<TEntity>(CancellationToken cancellationToken = default) where TEntity : class
-        {
-            return _context.Set<TEntity>().ToListAsync(cancellationToken);
-        }
+    public Task<List<TProjection>> List<TEntity, TProjection>(Query<TEntity, TProjection> query, CancellationToken cancellationToken = default) where TEntity : class
+    {
+        var queryProjection = QueryEvaluator.ApplyQuery(_context, query);
 
-        public Task<List<TProjection>> List<TEntity, TProjection>(Query<TEntity, TProjection> query, CancellationToken cancellationToken = default) where TEntity : class
-        {
-            var queryProjection = QueryEvaluator.ApplyQuery(_context, query);
+        return queryProjection.ToListAsync(cancellationToken);
+    }
 
-            return queryProjection.ToListAsync(cancellationToken);
-        }
+    public Task<TEntity?> FirstOrDefault<TEntity>(CancellationToken cancellationToken = default) where TEntity : class
+    {
+        return _context.Set<TEntity>().FirstOrDefaultAsync(cancellationToken);
+    }
 
-        public Task<TEntity?> FirstOrDefault<TEntity>(CancellationToken cancellationToken = default) where TEntity : class
-        {
-            return _context.Set<TEntity>().FirstOrDefaultAsync(cancellationToken);
-        }
+    public Task<TProjection?> FirstOrDefault<TEntity, TProjection>(Query<TEntity, TProjection> query, CancellationToken cancellationToken = default) where TEntity : class
+    {
+        var queryProjection = QueryEvaluator.ApplyQuery(_context, query);
 
-        public Task<TProjection?> FirstOrDefault<TEntity, TProjection>(Query<TEntity, TProjection> query, CancellationToken cancellationToken = default) where TEntity : class
-        {
-            var queryProjection = QueryEvaluator.ApplyQuery(_context, query);
+        return queryProjection.FirstOrDefaultAsync(cancellationToken);
+    }
 
-            return queryProjection.FirstOrDefaultAsync(cancellationToken);
-        }
+    public void Add<TEntity>(TEntity entity) where TEntity : class
+    {
+        _context.Add(entity);
+    }
 
-        public void Add<TEntity>(TEntity entity) where TEntity : class
-        {
-            _context.Add(entity);
-        }
+    public void Update<TEntity>(TEntity entity) where TEntity : class
+    {
+        _context.Update(entity);
+    }
 
-        public void Update<TEntity>(TEntity entity) where TEntity : class
-        {
-            _context.Update(entity);
-        }
-
-        public void Delete<TEntity>(TEntity entity) where TEntity : class
-        {
-            _context.Remove(entity);
-        }
+    public void Delete<TEntity>(TEntity entity) where TEntity : class
+    {
+        _context.Remove(entity);
     }
 }
