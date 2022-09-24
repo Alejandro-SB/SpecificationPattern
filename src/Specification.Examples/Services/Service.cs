241be43d6c4647cd884414efe62b720ec914336f
Alejandro <alejandro.sanchez.bastida@gmail.com>
2022-09-24 20:31:05 +0200

    Add service in example

---
+++ src/Specification.Examples/Services/Service.cs
@@ -0,0 +1,33 @@
+﻿using Specification.Abstractions;
+using Specification.Examples.Queries.People.GetPeopleByName;
+using Specification.Examples.Queries.Pets.GetCatsWithOwners;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Specification.Examples.Services;
+public class Service
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public Service(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public Task<List<GetCatsWithOwnersQueryDto>> GetCats()
+    {
+        var query = new GetCatsWithOwnersQuery();
+
+        return _unitOfWork.List(query);
+    }
+
+    public Task<GetPeopleByNameQueryDto?> GetFirstByName(string name)
+    {
+        var query = new GetPeopleByNameQuery(name);
+
+        return _unitOfWork.FirstOrDefault(query);
+    }
+}
